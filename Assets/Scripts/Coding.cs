8f4e609049ff96551783c61efa41b05f74771589
IYellALotA <166546144+IYellALotA@users.noreply.github.com>
2024-04-30 19:45:20 +0200

    Finished Flappy Bird v1.0.0
    Base game complited

---
+++ Assets/Scripts/Coding.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coding : MonoBehaviour
+{
+    [SerializeField] private float velocity = 1.5f;
+    [SerializeField] private float rotationSpeed = 10f;
+
+    private Rigidbody2D birdRb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        birdRb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if (Input.GetMouseButtonDown(0))
+        {
+            birdRb.velocity = Vector2.up * velocity;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        transform.rotation = Quaternion.Euler(0,0,birdRb.velocity.y * rotationSpeed);
+    }
+}
